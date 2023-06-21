aca7196dcc989302438be9c9b034cb7190a209ba
Winter <winteramlaw@gmail.com>
2023-06-20 23:23:21 -0400

    set up initial fetch call for shopify products on merch page with env variables

--- src/pages/MerchPage/Merch.js
+++ src/pages/MerchPage/Merch.js
@@ -1,34 +1,44 @@
 import React, { useState, useEffect } from 'react';
-// import Shopify from 'shopify-api-node';
+
+const apiKey = process.env.REACT_APP_SHOPIFY_API_KEY;
+const secretKey = process.env.REACT_APP_SHOPIFY_API_KEY;
+const accessToken = process.env.REACT_APP_SHOPIFY_ACCESS_TOKEN;
 
 const Merch = () => {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  // useEffect(() => {
-  //   const shopify = new Shopify({ shopName: 'your-store.myshopify.com', apiKey: 'your-api-key', password: 'your-api-password' });
-  //   shopify.product.list().then(products => {
-  //     setProducts(products);
-  //   }).catch(error => {
-  //     console.error(error);
-  //   });
-  // }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://bikethrasher.myshopify.com/admin/api/2023-01/products.json?ids=8251375386911', {
+        headers: {
+          'X-Shopify-Access-Token': `${accessToken}`,
+          'Content-Type': 'application/json',
+          'X-Shopify-API-Key': `${apiKey}`
+        }
+      });
+      const data = await response.json();
+      setProducts(data.products);
+    };
+    
+    fetchProducts();
+  }, []);
 
-  // return (
-  //   <div>
-  //     <h1>Merch</h1>
+  return (
+    <div>
+      <h1>Merch</h1>
 
-  //     <ul>
-  //       {products.map(product => (
-  //         <li key={product.id}>
-  //           <img src={product.images[0].src} alt={product.title} />
-  //           <h2>{product.title}</h2>
-  //           <p>{product.description}</p>
-  //           <span>{product.variants[0].price}</span>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.images[0].src} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <span>{product.variants[0].price}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Merch;
