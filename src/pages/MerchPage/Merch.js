f6b8306ec2a06dbd2bc4001840b249e39cdf32d2
Winter <winteramlaw@gmail.com>
2023-06-21 00:27:45 -0400

    initial styling for product cards on merch page

--- src/pages/MerchPage/Merch.js
+++ src/pages/MerchPage/Merch.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Client from 'shopify-buy';
+import styled from 'styled-components';
 
 const shopifyStoreAcessToken = process.env.REACT_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN
 const client = Client.buildClient({
   domain: 'bikethrasher.myshopify.com',
   storefrontAccessToken: shopifyStoreAcessToken
 });
+
 const Merch = () => {
   const [products, setProducts] = useState([]);
 
@@ -23,23 +25,82 @@ const Merch = () => {
     
     fetchProducts();
   }, []);
-
+  console.log(products[0].onlineStoreUrl);
   return (
-    <div>
+    <Container>
       <h1>Merch</h1>
 
-      <ul>
-        {/* {products.map(product => (
-          <li key={product.id}>
-            <img src={product.images[0].src} alt={product.title} />
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <span>{product.variants[0].price}</span>
-          </li>
-        ))} */}
-      </ul>
-    </div>
+      <ProductList>
+        {products && products.map(product => (
+          <ProductCard key={product.id} href={product.onlineStoreUrl}>
+            <ProductImage src={product.images[0].src} alt={product.title} />
+            <ProductInfo>
+              <ProductName>{product.title}</ProductName>
+              <ProductDesc>{product.description}</ProductDesc>
+              <ProductPrice>${product.variants[0].price.amount}</ProductPrice>
+            </ProductInfo>
+          </ProductCard>
+        ))}
+      </ProductList>
+    </Container>
   );
 };
 
 export default Merch;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductCard = styled.a`
+  display: block;
+  width: calc(33.33% - 20px);
+  margin: 10px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-decoration: none;
+  color: #333;
+
+  &:hover {
+    box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.2);
+    transform: translateY(-2px);
+  }
+`;
+
+const ProductImage = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
+  border-radius: 5px 5px 0 0;
+`;
+
+const ProductInfo = styled.div`
+  padding: 10px;
+`;
+
+const ProductName = styled.h2`
+  font-size: 16px;
+  margin: 0 0 5px 0;
+`;
+
+const ProductDesc = styled.p``;
+
+const ProductPrice = styled.span`
+  font-weight: bold;
+`;
